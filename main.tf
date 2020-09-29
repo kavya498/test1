c13b8d83ee27346a06e6bcb7f4400209cd37879c
kavya498 <46739084+kavya498@users.noreply.github.com>
2020-09-29 22:17:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,31 +3,6 @@ variable "ic_api_key"{}
 provider "ibm"{
   ibmcloud_api_key=var.ic_api_key
 }
-data "ibm_resource_group" "web_group" {
-  name = "Default"
-}
-data "ibm_cis" "web_instance" {
-  name              = "CISTest"
-  resource_group_id = data.ibm_resource_group.web_group.id
-}
-data "ibm_cis_domain" "web_domain" {
-  cis_id = data.ibm_cis.web_instance.id
-  domain = "cis-terraform.com"
-}
-# resource "ibm_cis_dns_record" "examplednsa" {
-#   cis_id           = data.ibm_cis.web_instance.id
-#   domain_id        = data.ibm_cis_domain.web_domain.id
-#   name      = var.name
-#   content   = "1.2.3.4"
-#   type      = "A"
-#   proxied = true
-# }
-resource "ibm_cis_dns_record" "dns_mx_1" {
-  cis_id           = data.ibm_cis.web_instance.id
-  domain_id        = data.ibm_cis_domain.web_domain.id
-  name      = "sample"
-  ttl       = 3600
-  type      = "MX"
-  priority  = "0"
-  content   = "xyz.mail.protection.outlook.com"
+data "ibm_container_cluster_config" "cluster_config" {
+    cluster_name_id  = "btplfs0d0noh9gcrniog"
 }
