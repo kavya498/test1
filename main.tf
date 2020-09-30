af520bd70672f7f1a44721c6b33ce706732af137
kavya498 <46739084+kavya498@users.noreply.github.com>
2020-09-30 08:42:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ variable "ic_api_key"{}
 provider "ibm"{
   ibmcloud_api_key=var.ic_api_key
 }
+data "ibm_resource_group" "resource_group" {
+  name = var.resource_group
+}
 data "ibm_container_cluster_config" "cluster_config" {
     cluster_name_id  = "btplfs0d0noh9gcrniog"
+   resource_group_id = data.ibm_resource_group.resource_group.id
 }
