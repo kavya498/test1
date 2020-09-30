e4126c07b2dff38337bc592df1672c7bbc0e7239
kavya498 <46739084+kavya498@users.noreply.github.com>
2020-09-30 08:58:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,5 +8,6 @@ data "ibm_resource_group" "resource_group" {
 }
 data "ibm_container_cluster_config" "cluster_config" {
     cluster_name_id  = "btplfs0d0noh9gcrniog"
-   resource_group_id = data.ibm_resource_group.resource_group.id
+    resource_group_id = data.ibm_resource_group.resource_group.id
+    config_dir = "/tmp"
 }
