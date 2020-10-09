68d0eef2ab52882270c16652a12f787a26336f1b
kavya498 <46739084+kavya498@users.noreply.github.com>
2020-10-09 13:46:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,27 @@
 variable "ic_api_key"{}
-variable "resource_group"{}
+
 provider "ibm"{
+    region="eu-de"
   ibmcloud_api_key=var.ic_api_key
 }
-data "ibm_resource_group" "resource_group" {
-  name = var.resource_group
+data "ibm_resource_group" "test_acc" {
+	name = "Default"
 }
-data "ibm_container_cluster_config" "cluster_config" {
-    cluster_name_id  = "btplfs0d0noh9gcrniog"
-    resource_group_id = data.ibm_resource_group.resource_group.id
-    config_dir = "/tmp"
+resource "ibm_database" "rabit" {
+    resource_group_id = data.ibm_resource_group.test_acc.id
+    name              = "rabbit-test-db"
+    service           = "messages-for-rabbitmq"
+    plan              = "standard"
+    location          = "eu-de"
+    adminpassword                = "password12"
+    members_memory_allocation_mb = 6144
+    members_disk_allocation_mb   = 6144
+    users {
+        name     = "user123"
+        password = "password12"
+    }
+    whitelist {
+        address     = "172.168.1.2/32"
+        description = "desc1"
+    }
 }
