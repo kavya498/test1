b279ad71e6e07e2f8159cc1617832fd3a65f66e5
kavya498 <46739084+kavya498@users.noreply.github.com>
2020-10-14 17:21:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,18 +4,17 @@ provider "ibm"{
     region="eu-de"
   ibmcloud_api_key=var.ic_api_key
 }
-data "ibm_resource_group" "test_acc" {
-	name = "Default"
+resource "random_string" "name" {
+  length  = 8
+  upper   = false
+  lower   = true
+  number  = false
+  special = false
 }
-resource "ibm_database" "redis_standard" {
-    name              = "rabbit-backup"
-    service           = "messages-for-rabbitmq"
-    plan              = "standard"
-    location          = "eu-de"
-    backup_id           = "crn:v1:bluemix:public:messages-for-rabbitmq:eu-de:a/4448261269a14562b839e0a3019ed980:ba99078e-cf2c-4727-9a7f-bbb91d131fe8::"
-    service_endpoints   = "private"
-    resource_group_id   = data.ibm_resource_group.test_acc.id
-    tags = ["oss","terraform","redis_standard"]
-    members_memory_allocation_mb = 4096
-    members_disk_allocation_mb   = 2048
+
+resource "ibm_database" "database" {
+  name              = random_string.name.id
+  plan              = "standard"
+  location          = "eu-de"
+  service           = "databases-for-etcd"
 }
