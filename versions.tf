4fcaeaabbb61bb44d83efaae22085d859789188c
kavya498 <46739084+kavya498@users.noreply.github.com>
2021-04-20 12:08:04 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,11 +2,3 @@
 # IBM Cloud Key Management Services Provisioning and Managing Keys
 # Copyright 2021 IBM
 #########################################################################################
-terraform {
-  required_providers {
-    ibm = {
-      source = "IBM-Cloud/ibm"
-      version= "1.23.0"
-    }
-  }
-}
