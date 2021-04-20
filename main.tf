36ea8fba01bb4a1b303f4027f9e8a706efc80bb6
kavya498 <hkavya26@in.ibm.com>
2021-04-20 11:47:49 +0530

    Test Modules

--- main.tf
+++ main.tf
@@ -1,45 +1,26 @@
-variable "ic_api_key"{}
-
-provider "ibm"{
-    region="eu-de"
-  ibmcloud_api_key=var.ic_api_key
-}
-data "ibm_resource_group" "test_acc"{
-    name ="Default"
+#########################################################################################
+# IBM Cloud Key Management Services Provisioning and Managing Keys
+# Copyright 2021 IBM
+#########################################################################################
+data "ibm_resource_group" "resource_group" {
+  name = var.resource_group
 }
 
-resource "ibm_resource_instance" "cos_instance" {
-  name              = "cos-instance-sch"
-  resource_group_id = data.ibm_resource_group.test_acc.id
-  service           = "cloud-object-storage"
-  plan              = "standard"
-  location          = "global"
-  tags = concat(var.database_tags, list("GIT_VERSION:1.0"))
-    parameters = {
-    "HMAC" = true
-    }
-    timeouts {
-        create = "25m"
-        update = "15m"
-        delete = "15m"
-    }
+module "kms_instance" {
+  source                 = "../modules/instance"
+  resource_group_id      = data.ibm_resource_group.resource_group.id
+  service_name           = var.service_name
+  location               = var.location
+  plan                   = "tiered-pricing"
+  tags                   = var.tags
+  allowed_network_policy = var.allowed_network_policy
 }
 
-resource "ibm_database" "pg" {
-    resource_group_id = data.ibm_resource_group.test_acc.id
-    name              = "pg-test-schematics"
-    service           = var.database_type
-    plan              = "standard"
-    location          = "eu-de"
-    service_endpoints= var.service_end_points
-    version= var.database_version
-    adminpassword                = var.admin_password
-    members_memory_allocation_mb = var.ram_size
-    members_disk_allocation_mb   = var.disk_size
-    tags = concat(var.database_tags, list("GIT_VERSION:1.0"))
-    timeouts {
-        create = "120m"
-        update = "60m"
-        delete = "15m"
-    }
+module "kms_key" {
+  source                 = "../modules/key"
+  kms_instance_guid      = module.kms_instance.kms_instance_output.guid
+  name                   = var.name
+  standard_key_type      = var.standard_key_type
+  force_delete           = var.force_delete
+  network_access_allowed = var.network_access_allowed
 }
---
+++ versions.tf
@@ -0,0 +1,12 @@
+#########################################################################################
+# IBM Cloud Key Management Services Provisioning and Managing Keys
+# Copyright 2021 IBM
+#########################################################################################
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version= "1.23.0"
+    }
+  }
+}
