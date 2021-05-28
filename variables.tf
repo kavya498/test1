1e0f3ea592dd153a595ac8b1a6d3155b953f50b8
kavya498 <46739084+kavya498@users.noreply.github.com>
2021-05-28 09:46:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,9 +2,4 @@
 # IBM Cloud Key Management Services Provisioning and Managing Keys
 # Copyright 2021 IBM
 #########################################################################################
-
-variable "resource_group" {
-  type        = string
-  description = "Resource group of instance"
-}
 variable "ibmcloud_api_key"{}
