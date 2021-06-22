b5d8fe9d8c9f3b15dcf69fe9bd418a128562f626
kavya498 <46739084+kavya498@users.noreply.github.com>
2021-06-22 22:57:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,4 +2,4 @@
 # IBM Cloud Key Management Services Provisioning and Managing Keys
 # Copyright 2021 IBM
 #########################################################################################
-variable "ibmcloud_api_key"{}
+
