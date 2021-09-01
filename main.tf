e4fe83d74b67a0934dfc8831a8d09d15a939a189
kavya498 <46739084+kavya498@users.noreply.github.com>
2021-09-01 14:44:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,46 +1,31 @@
 terraform {
-  required_version = ">=0.13"
   required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
     ibm = {
       source = "ibm-cloud/ibm"
+      version = "~> 1.26.0"
     }
   }
 }
-provider "google" {
-  project = var.gcp_project
-  region  = var.gcp_region
-  credentials = var.gcp_credentials
+data "ibm_container_cluster_config" "cluster_config" {
+  cluster_name_id = var.cluster_k8s
+  resource_group_id = var.cluster_k8s_rg
+  admin           = true
 }
-provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-}
-variable "ibmcloud_api_key"{}
 
-data "ibm_resource_group" "rg"{
-  name= "Default"
+
+provider "kubernetes" {
+  host                   = data.ibm_container_cluster_config.cluster_config.host
+  client_certificate     = data.ibm_container_cluster_config.cluster_config.admin_certificate
+  client_key             = data.ibm_container_cluster_config.cluster_config.admin_key
+  cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
 }
-data "google_compute_zones" "available" {
-  project = var.gcp_project
-  region  = var.gcp_region
-}
-output "zone" {
-  value = data.google_compute_zones.available
-}
-output "rg" {
-  value = data.ibm_resource_group.rg
-}
-variable "gcp_project" {
-  description = "GCP Project ID"
-  type        = string
-}
-variable "gcp_region" {
-  description = "Google Region"
-  type        = string
-}
-variable "gcp_credentials" {
-  description ="Either the path to or the contents of a service account key file in JSON format."
-  type =string
+
+
+resource "kubernetes_namespace" "namespacek8s" {
+  metadata {
+    labels = {
+      namespace_name = "test"
+    }
+    name = "test"
+  }
 }
