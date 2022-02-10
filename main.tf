62401ec97cf9978b5b9ead556dbd2935b9bd106d
Kavya Handadi <46739084+kavya498@users.noreply.github.com>
2022-02-11 00:07:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,54 +1,15 @@
-terraform {
-  required_providers {
-    ibm = {
-      source = "ibm-cloud/ibm"
-    }
-  }
+resource "ibm_database" "icd_mongo" {
+  name              = "mongo-stage"
+  service           = "databases-for-mongodb"
+  plan              = "standard"
+  version           = "4.2"
+  location          = "eu-de"
+#   tags              = var.tags
+  adminpassword                = "Passw0rd01"
+  members_memory_allocation_mb = 3072  # 1GB  per member
+  members_disk_allocation_mb   = 61440 # 20GB per member
+  members_cpu_allocation_count = 0
 }
-################################
-## provider initialisation ##
-################################
-provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = var.region
+provider "ibm"{
+    region = "eu-de"
 }
-################################
-## variables
-################################
-variable "ibmcloud_api_key" {}
-variable "resource_prefix" {
-  default = "test"
-}
-variable "region" {
-  default = "us-south"
-}
-################################
-## rg, vp, subnet resources
-################################
-resource "ibm_resource_group" "resource_group" {
-  name = "${var.resource_prefix}-group"
-}
-resource "ibm_is_vpc" "vpc" {
-  name           = "${var.resource_prefix}-vpc"
-  resource_group = ibm_resource_group.resource_group.id
-}
-resource "ibm_is_subnet" "subnet" {
-  name                     = "${var.resource_prefix}-subnet"
-  vpc                      = ibm_is_vpc.vpc.id
-  zone                     = "${var.region}-1"
-  total_ipv4_address_count = 256
-  resource_group           = ibm_resource_group.resource_group.id
-}
-/*
-resource "ibm_container_vpc_cluster" "cluster" {
-	name              = "vpc-cluster"
-	vpc_id            = ibm_is_vpc.vpc.id
-	flavor            = "cx2.2x4"
-	worker_count      = 1
-  resource_group_id = ibm_resource_group.resource_group.id
-	zones {
-		 subnet_id = ibm_is_subnet.subnet.id
-		 name      = "us-south-1"
-	}
-  }
-*/
