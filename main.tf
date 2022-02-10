bfa44e4303051bb15db9767a3d5e25ad769ec8f4
Kavya Handadi <46739084+kavya498@users.noreply.github.com>
2022-02-11 00:14:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "ibm_database" "icd_mongo" {
   adminpassword                = "Passw0rd01"
   members_memory_allocation_mb = 3072  # 1GB  per member
   members_disk_allocation_mb   = 61440 # 20GB per member
-  members_cpu_allocation_count = 0
+  members_cpu_allocation_count = 3
 }
 provider "ibm"{
     region = "eu-de"
