8411d9a9ce40ceb760e6e2b964cbec2a00019031
Kavya Handadi <46739084+kavya498@users.noreply.github.com>
2022-02-14 13:39:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,26 @@
+
+variable "key" {}
+
 resource "ibm_database" "icd_mongo" {
-  name              = "mongo-stage"
+  name              = "mongo-1"
   service           = "databases-for-mongodb"
   plan              = "standard"
-  version           = "4.2"
-  location          = "eu-de"
+  location          = "eu-gb"
 #   tags              = var.tags
   adminpassword                = "Passw0rd01"
   members_memory_allocation_mb = 3072  # 1GB  per member
   members_disk_allocation_mb   = 61440 # 20GB per member
-  members_cpu_allocation_count = 9
+  members_cpu_allocation_count = 0
 }
 provider "ibm"{
     region = "eu-gb"
   ibmcloud_api_key = var.key
 }
-variable "key" {}
+
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+    }
+  }
+}
