cc0d1ecbefe6eeb5b70ef2cfc9720ae00f976029
Kavya Handadi <46739084+kavya498@users.noreply.github.com>
2022-03-02 22:51:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
-	  version = "1.38.2"
+
     }
   }
 }
@@ -14,18 +14,6 @@ variable ibmcloud_api_key {
   description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
   type        = string
 }
-
-resource "ibm_database" elastic {
-  name              = "es"
-  plan              = "standard"
-  location          = "eu-de"
-  service           = "databases-for-elasticsearch"
-  resource_group_id = "766f3584b2c840ee96d856bc04551da8"
-  // Must Be an Increment of 384
-  members_memory_allocation_mb = 18048
-  // Must be an Increment of 3072
-  members_disk_allocation_mb   = 451584
-  adminpassword = "kavya12345"
-  key_protect_key = "crn:v1:bluemix:public:kms:us-south:a/4448261269a14562b839e0a3019ed980:adb92f84-eb19-4b6b-8afd-c496af08f7ba:key:d7ad00ed-9da3-4b4c-aea0-3fb71c1b8acc"
-  key_protect_instance = "crn:v1:bluemix:public:kms:us-south:a/4448261269a14562b839e0a3019ed980:adb92f84-eb19-4b6b-8afd-c496af08f7ba::"
+resource "ibm_is_vpc" "example" {
+  name = "example-vpc"
 }
