d2038dc5e261683751029cbeadeee9b5e5cdac03
Kavya Handadi <46739084+kavya498@users.noreply.github.com>
2022-03-25 14:00:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,23 +14,27 @@ variable ibmcloud_api_key {
   description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
   type        = string
 }
-
+data "ibm_resource_group" "group" {
+  name = "pte-interns"
+}
 resource "ibm_database" "icd_mongo" {
   name              = "mongo"
   service           = "databases-for-mongodb"
   plan              = "standard"
   version           = "4.2"
+  resource_group_id            = data.ibm_resource_group.group.id
   location          = "eu-de"
   adminpassword                = "Passw0rd01"
   members_memory_allocation_mb = 3072  # 1GB  per member
   members_disk_allocation_mb   = 61440 # 20GB per member
 }
 resource "ibm_database" "icd_mongo1" {
-	depends_on =[ibm_database.icd_mongo]
+  depends_on =[ibm_database.icd_mongo]
   name              = "mongo1"
   service           = "databases-for-mongodb"
   plan              = "standard"
   version           = "4.2"
+  resource_group_id            = data.ibm_resource_group.group.id
   location          = "eu-de"
   adminpassword                = "Passw0rd01"
   members_memory_allocation_mb = 3072  # 1GB  per member
@@ -40,6 +44,7 @@ resource "ibm_database" "icd_mongo2" {
 	depends_on =[ibm_database.icd_mongo1]
   name              = "mongo2"
   service           = "databases-for-mongodb"
+  resource_group_id            = data.ibm_resource_group.group.id
   plan              = "standard"
   version           = "4.2"
   location          = "eu-de"
